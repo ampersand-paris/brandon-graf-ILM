f04fdf819d05491d03a09ae8a7070c11fc6640f2
ampersand-paris <ampersand.s.paris@gmail.com>
2023-03-14 10:02:12 -0400

    about page finished / mobile responsive / navigation responsive

--- brandon-graf-frontend/src/Dance/About/about.js
+++ brandon-graf-frontend/src/Dance/About/about.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import useFetch from "../../useFetch";
 import DanceMenu from "../../Partials/Dance/danceMenu";
+import ReactMarkdown from 'react-markdown'
 
 function About() {
 
   const { isLoading, error, data } = useFetch(`${process.env.REACT_APP_BACKEND}api/about`)
+  console.log(data)
 
   if (isLoading) {
     return <div className="App"><h1>Loading...</h1></div>
@@ -17,20 +19,24 @@ function About() {
   return (
     <>
       <div className="intro--container bg--lilac">
-        <img src="../Images/Portraits/Brandon_Headshot.png" />
+        <img className="headshot" src="../Images/Portraits/Brandon_Headshot.png" />
         <div className="intro--text">          
           <h1 className="page--intro">{data.data.attributes.Intro}</h1>
           <p>{data.data.attributes.Intro_Paragraph}</p>
         </div>
       </div>
       <div className="body--container">
-        <div>
-          <img src="../Images/Headings/Biography.png" />
-          <p className="body--text">{data.data.attributes.Biography}</p>
+        <div className="grid--container">
+          <div>
+            <img className="about--header" src="../Images/Headings/Biography.png" />
+          </div>
+          <p className="body--text"><ReactMarkdown>{data.data.attributes.Biography}</ReactMarkdown></p>
         </div>
-        <div>
-          <img src="../Images/Headings/Biography.png" />
-          <p className="body--text">{data.data.attributes.Artist_Statement}</p>
+        <div className="grid--container">
+          <div>
+            <img className="about--header" src="../Images/Headings/Biography.png" />
+          </div>
+          <p className="body--text"><ReactMarkdown>{data.data.attributes.Artist_Statement}</ReactMarkdown></p>
         </div>
       </div>
     </>
